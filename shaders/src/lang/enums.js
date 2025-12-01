7d665d537db01622f8209a04fe947fb6720e59a2
Alex Ayars <aayars@gmail.com>
2025-12-01 12:25:23 -0700

    shaders wip

--- shaders/src/lang/enums.js
+++ shaders/src/lang/enums.js
@@ -1,16 +1,59 @@
 const legacyEnums = {};
-const mutableEnums = legacyEnums;
+let mutableEnums = legacyEnums;
 let frozenEnums = null;
 
-function cloneEnumTree(source, mergeEnumsFn) {
+/**
+ * Deep merge source into target, recursively merging nested objects
+ * Creates new objects as needed to avoid mutating frozen objects
+ */
+function deepMerge(target, source) {
+  if (!source || typeof source !== 'object') return target;
+  
+  for (const key of Object.keys(source)) {
+    const sourceVal = source[key];
+    const targetVal = target[key];
+    
+    // If both are plain objects (not arrays, not enum entries with 'type'), recurse
+    if (sourceVal && typeof sourceVal === 'object' && !Array.isArray(sourceVal) &&
+        targetVal && typeof targetVal === 'object' && !Array.isArray(targetVal) &&
+        !('type' in sourceVal)) {
+      // Create a new object if target is frozen
+      if (Object.isFrozen(targetVal)) {
+        target[key] = deepMerge({ ...targetVal }, sourceVal);
+      } else {
+        deepMerge(targetVal, sourceVal);
+      }
+    } else {
+      // Otherwise just assign (overwrites or adds)
+      target[key] = sourceVal;
+    }
+  }
+  return target;
+}
+
+/**
+ * Deep clone an object
+ */
+function deepClone(obj) {
+  if (!obj || typeof obj !== 'object') return obj;
+  if (Array.isArray(obj)) return obj.map(deepClone);
+  
   const clone = {};
-  if (mergeEnumsFn) {
-      mergeEnumsFn(clone, source);
-  } else {
-      // Simple shallow merge if no function provided
-      Object.assign(clone, source);
+  for (const key of Object.keys(obj)) {
+    clone[key] = deepClone(obj[key]);
   }
   return clone;
+}
+
+function cloneEnumTree(source, mergeEnumsFn) {
+  if (mergeEnumsFn) {
+    const clone = {};
+    mergeEnumsFn(clone, source);
+    return clone;
+  } else {
+    // Deep clone for frozen copy
+    return deepClone(source);
+  }
 }
 
 function deepFreezeEnumTree(node) {
@@ -34,7 +77,8 @@ export async function mergeIntoEnums(source, mergeEnumsFn) {
   if (mergeEnumsFn) {
       mergeEnumsFn(mutableEnums, source);
   } else {
-      Object.assign(mutableEnums, source);
+      // Use deep merge to properly handle nested namespace paths
+      deepMerge(mutableEnums, source);
   }
   rebuildFrozenEnums(mergeEnumsFn);
   return frozenEnums;
